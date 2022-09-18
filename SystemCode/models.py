e844e65cba5884f6e2a1bd46a993ffa0bc06a6d6
Alan Wang <101057339+wangsong300@users.noreply.github.com>
2022-09-18 17:32:56 +0800

    updated with the base model
    feat: using base model

--- SystemCode/models.py
+++ SystemCode/models.py
@@ -7,6 +7,8 @@ from tensorflow.keras.layers import Dropout
 from tensorflow.keras.layers import Flatten
 from tensorflow.keras.layers import Conv2D
 from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras import optimizers
+from tensorflow.keras import regularizers
 # SingletonMeta
 
 
@@ -20,26 +22,46 @@ class SingletonMeta(type):
         return cls._instances[cls]
 
 
-filepath = "./models/jeremy_2022-09-13_20-51-43.hdf5"
+filepath = "./models/base_128_200_64_1_2022-09-18_17-14-53.hdf5"
 types = ["arowana", "betta", "goldfish", "luohan", "others", "I cannot tell"]
 UPLOAD_FOLDER = "./static/images"
+
+modelname = 'base'
+
+IMGSIZE = 128
+EPOCHS = 200
+BATCH_SIXE = 64
+OPT_IDX = 1
+
+modelname = modelname+"_"+str(IMGSIZE)+"_" + \
+    str(EPOCHS)+"_"+str(BATCH_SIXE)+"_"+str(OPT_IDX)
+optmzs = ['adam', optimizers.RMSprop(learning_rate=0.0001)]
+optmz = optmzs[OPT_IDX]
 
 
 def createModel():
     model = Sequential()
-    model.add(Conv2D(30, (5, 5), input_shape=(180, 180, 3), activation='relu'))
+    model.add(Conv2D(64, (5, 5), padding='same', input_shape=(
+        IMGSIZE, IMGSIZE, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Conv2D(40, (5, 5), activation='relu'))
+    model.add(Conv2D(32, (3, 3), padding='same', activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(32, (3, 3), padding='same', activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.1))
+    model.add(Conv2D(32, (3, 3), padding='same', activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.15))
+    model.add(Conv2D(32, (3, 3), padding='same', activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.2))
     model.add(Flatten())
-    model.add(Dense(128, activation='relu'))
-    model.add(Dense(40, activation='relu'))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dense(10, activation='relu'))
     model.add(Dense(4, activation='softmax'))
     model.compile(loss='categorical_crossentropy',
-                  optimizer='adam', metrics=['accuracy'])
+                  optimizer=optmz, metrics=['accuracy'])
     return model
- # This is used for final testing
 
 
 class Classifier(metaclass=SingletonMeta):
@@ -55,7 +77,7 @@ class Classifier(metaclass=SingletonMeta):
 
         print(UPLOAD_FOLDER+"/"+image)
         img = cv2.imread(str(UPLOAD_FOLDER+"/"+image))
-        resized_img = cv2.resize(img, (180, 180))
+        resized_img = cv2.resize(img, (IMGSIZE, IMGSIZE))
         resized_img = np.array([resized_img])
         resized_img = resized_img / 255
         result = self.modelGo.predict(resized_img)
